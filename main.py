3ba8b674b377f7426c70f0913b7683457b69bd74
kcrong <hyunwoo@kim82536.pe.kr>
2015-09-16 17:36:36 +0900

    tcpdump를 이용해 프로세스가 사용중인 포트를 지나는 패킷을 캡쳐합니다.

--- main.py
+++ main.py
@@ -27,13 +27,49 @@ def get_process_port(pid):
         port_list.append(result[1])
 
     del addr_list
+    try:
+        if port_list[len(port_list) - 1] == '*':
+            port_list.pop()
+    except IndexError:
+        print "PROCESS ISN'T USING PORT! OR, WRONG PID"
+        exit(1)
     return port_list
 
+
+def make_command(interface, port_list, file_name):
+    comm = "sudo tcpdump -i %s -w %s " % (interface, file_name)
+
+    for port in port_list:
+        comm += "port %s or " % port
+
+    comm = comm[:len(comm) - 4]
+
+    return comm
+
+
 if __name__ == "__main__":
+    DEBUG = False
+
+    # get pid from argv
     try:
         pid = sys.argv[1]
+        interface = sys.argv[2]
+        file_name = sys.argv[3]
     except IndexError:
-        print 'Argv Error'
-        exit()
+        print '%s [PID] [INTERFACE] [FILE_NAME]' % sys.argv[0]
+        print "Ex) %s 1234 eth0 packets.pcap" % sys.argv[0]
+        if DEBUG is True:
+            pid = 6191
+            interface = "wlan0"
+        else:
+            exit()
     port_list = get_process_port(pid)
 
+    comm = make_command(interface, port_list, file_name)
+
+    import os
+
+    os.system(comm)
+
+# sudo tcpdump -w packets.pcap -i eth0 port 8080
+# sudo tcpdump -i wlan0 -w asdf.pcap port 8080 or port 80
